9473c8b14091e25c42b5de6f226c739b3f515c1a
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2021-03-08 18:49:08 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import speedtest
+import time
+
+list_download_speed = []
+list_upload_speed = []
+
+for i in range(5):
+    st = speedtest.Speedtest()
+    download_speed = round(st.download()/1000000, 2)
+    list_download_speed.append(download_speed)
+    upload_speed = round(st.upload()/1000000, 2)
+    list_upload_speed.append(upload_speed)
+    time.sleep(60)
+    print(list_download_speed)
+    print(list_upload_speed)
+
+x = [1,2,3,4,5]
+y = [1,2,3,4,5]
+plt.plot(x, list_download_speed, label = "Download Speed")
+plt.plot(y, list_upload_speed, label = "Upload Speed")
+plt.title("Speed")
+plt.legend()
+plt.show()
